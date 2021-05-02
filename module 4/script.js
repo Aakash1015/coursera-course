55e871ceeb7a1de12a973b80979e67d79133af3b
Aakash1015 <78781468+Aakash1015@users.noreply.github.com>
2021-05-03 00:59:35 +0530

    Add files via upload

---
+++ module 4/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+var names = ["Aakash", "Saras", "Shivam", "Jason", "Paul", "Frank", "Rohit", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
